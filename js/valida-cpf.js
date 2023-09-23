c6718f0a69dcc232afaa1df7f0b0fb30fe7dc615
Julio Gazoli <julio_gazoli@hotmail.com>
2023-09-23 10:46:53 -0300

    CPF: primeiro dígito

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,7 +1,8 @@
 export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
-    
-    console.log(validaNumerosRepetidos(cpf));
+    validaNumerosRepetidos(cpf);
+    validaPrimeiroDigito(cpf);
+    console.log();
 }
 
 function validaNumerosRepetidos(cpf) {
@@ -19,4 +20,22 @@ function validaNumerosRepetidos(cpf) {
     ]
 
     return numeroRepetidos.includes(cpf);
+}
+
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for(let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[9];
 }
